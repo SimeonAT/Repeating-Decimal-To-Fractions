d91a5094b2e3cbfdfb32c7c0b61a1210b69362b8
Simeon Tran <ilikedragonslol@gmail.com>
2020-03-10 17:00:53 -0700

    Update Draft 6.cpp

--- Draft 6.cpp
+++ Draft 6.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <float.h>
+#include <iomanip>
 using namespace std;
 
-double* simplify(double numerator, double denominator) {
+long int* simplify(long int numerator, long int denominator) {
 
 	/* - Check if each number from 1-100 is divisible by both numerator and denominator
 		 - if so -> divide by that number
 	   - repeat until there are no numbers from 1-100 which is divisible by both numerator and denominator */
 
-	double top = numerator;
-	double bottom = denominator;
+	long int top = numerator;
+	long int bottom = denominator;
 
 	for (int num = 2; num < 1000; num++) {
-		// Temporarily converts top and bottom and into to compare modulo
-		int int_top = (int)top;
-		int int_bottom = (int)bottom;
 
-		if ((int_top % num == 0) && (int_bottom % num == 0)) {
+		// debug line 
+		cout << "Looking at num = " << num << endl;
+		cout << "fmod(top, num) = " << fmod(top, num) << endl;  
+		cout << "fmod(bottom, num) = " << fmod(bottom, num) << endl;  
+		cout << "top / num: " << top / num << endl;
+		cout << "bottom / num: " << bottom / num << endl;
+		// end debug lines 
+
+		if ((fmod(top, num) == 0) && (fmod(bottom, num) == 0)) {
+			cout << "Simplifying by: " << num << endl; // debug line 
+
 			top /= num;
 			bottom /= num;
 			num = 2;  // resets the loop 
 		}
+
+		// debug lines
+		cout << "current fraction: " << top << "/" << bottom << endl;  
+		cout << endl;
 	}
 
-	double simplified_fraction[] = { top, bottom };
+	long int simplified_fraction[] = { top, bottom };
 	return simplified_fraction;
 }
 
@@ -36,7 +49,7 @@ std::string fractions(std::string decimal) {
 	   - decimal part is partially repeating */
 	int parenthesis = 0, period = 0;
 	string repeat, non_repeat, repeat_denominator;
-	double repeat_top, repeat_bottom, non_repeat_top, non_repeat_bottom;
+	long int repeat_top, repeat_bottom, non_repeat_top, non_repeat_bottom;
 
 	for (int i = 0; i < decimal.length(); i++) {
 		if (decimal[i] == '(') { parenthesis = i; }
@@ -68,18 +81,23 @@ std::string fractions(std::string decimal) {
 			// convert repeat and repeat_denominator to int and put it into an array 
 			stringstream top(repeat); top >> repeat_top;
 			stringstream bottom(repeat_denominator); bottom >> repeat_bottom;
-			double repeat_fraction[2] = { repeat_top, repeat_bottom };
- 
+			long int repeat_fraction[2] = { repeat_top, repeat_bottom };
+
 			// conver non_repeat to int, then change non_repeat to fraction with repeat_bottom as denominator
 			non_repeat_top = stoi(non_repeat); non_repeat_bottom = repeat_bottom;
-			double non_repeat_fraction[2] = { non_repeat_top * non_repeat_bottom , non_repeat_bottom };
+			long int non_repeat_fraction[2] = { non_repeat_top * non_repeat_bottom , non_repeat_bottom };
 
 			// add non_repeat_fraction and repeat_fraction, and simplify it after 
-			double fraction[2] = { repeat_fraction[0] + non_repeat_fraction[0], repeat_fraction[1] };
-  
+			long int fraction[2] = { repeat_fraction[0] + non_repeat_fraction[0], repeat_fraction[1] };
+
+			// debug lines 
+			cout << setprecision(20) << fraction[0] << "/" << setprecision(20) << fraction[1] << endl;
+			cout << fraction[0] << "/" << fraction[1] << endl;
+
+
 			// simply fraction and convert it to string 
-			double* simplified = simplify(fraction[0], fraction[1]);
-			double temp_array[2] = { simplified[0], simplified[1] };
+			long int* simplified = simplify(fraction[0], fraction[1]);
+			long int temp_array[2] = { simplified[0], simplified[1] };
 
 			string numerator = to_string(temp_array[0]);
 			string denominator = to_string(temp_array[1]);
@@ -99,3 +117,17 @@ int main() {
 	cout << fractions("0.(052631578947368421)") << endl;
 	return 0;
 }
+
+
+/* NOTES: 
+- use long int 
+- use strstr() and strtol() to get index of "(" and "."
+
+"Again, whenever you're writing algorithms it is 
+always good to write and test your ideas on the python terminal. 
+It's easier to convert it to C++ code after that. Directly writing C/C++ code 
+(if you're inexperienced) is not a good idea since you'll spend a large part 
+of the work in debugging your code."
+
+
+*/
